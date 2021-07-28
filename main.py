b0e0eeb2497bb66b5c33c70e14c53dda6e3144b1
Chris Molanus <chrismolanus@gmail.com>
2021-07-28 09:51:56 +0200

    use max distance of K Nearest neighbors to calabrate distance in street dimention

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+from typing import Dict
+
 import osmnx as ox
 import networkx as nx
 
-
 import matplotlib.pyplot as plt
 import numpy as np
+import random as rd
 
 from k_means_constrained import KMeansConstrained
-
+from sklearn.neighbors import KDTree
 place_name = "Hoeilaart, Belgium"
 
 
@@ -26,6 +28,21 @@ building_centroids = list()
 for _, building in building_gdf.iterrows():
     centroid = building['geometry'].centroid
     building_centroids.append([centroid.xy[0][0], centroid.xy[1][0]])
+
+# Get max distance of nearest building to get a feel
+tree = KDTree(building_centroids, leaf_size=1000)
+dist, ind = tree.query(building_centroids[:50], k=3)
+
+# Get Building centroids with street dimension
+building_centroids = list()
+street_counter: Dict[str, float] = dict()
+for _, building in building_gdf.iterrows():
+    centroid = building['geometry'].centroid
+    street = building["addr:street"]
+    if street not in street_counter:
+        street_counter[street] = len(street_counter.keys()) * dist.max() * 2
+    street_counter[street]
+    building_centroids.append([centroid.xy[0][0], centroid.xy[1][0], street_counter[street]])
 
 # Form clusters of buildings
 clf = KMeansConstrained(n_clusters=77, size_min=50, size_max=60, random_state=0)
@@ -54,3 +71,5 @@ fig, ax = ox.plot_graph(G1, bgcolor='white', edge_color=ec,
 # add building footprints in 50% opacity with cluster colors
 ox.plot_footprints(building_gdf, ax=ax, color=label_colors, alpha=0.5)
 plt.show()
+
+
