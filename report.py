d105aeaa2a63a14c13595f691377f8c19bbd9783
Chris Molanus <chris.molanus@cognizant.com>
2021-10-08 09:46:59 +0200

    test: get report code to work if objects are not filled since we are still developing them

--- report.py
+++ report.py
@@ -1,9 +1,12 @@
 from typing import Tuple
 
+import geopandas
 from matplotlib.pyplot import plot
 from pandas import DataFrame
 
 from costs import DetailedCost
+
+import osmnx as ox
 
 
 class DetailedReport:
@@ -12,18 +15,20 @@ class DetailedReport:
         self.cost_table = costs_table
 
 
-def get_detailed_report(detailed_cost: DetailedCost) -> DetailedReport:
-    # Give different things different colours
-    ec = ['yellow' if 'highway' in d else
-          'gray' if 'trench_crossing' in d else
-          'blue' if 'trench' in d
-          else 'red'
-          for _, _, _, d in g_box.edges(keys=True, data=True)]
+def get_detailed_report(detailed_cost: DetailedCost, building_gdf: geopandas.GeoDataFrame) -> DetailedReport:
+    if detailed_cost.fiber_network.fiber_network is not None:
+        g_box = detailed_cost.fiber_network.fiber_network
+        # Give different things different colours
+        ec = ['yellow' if 'highway' in d else
+              'gray' if 'trench_crossing' in d else
+              'blue' if 'trench' in d
+              else 'red'
+              for _, _, _, d in g_box.edges(keys=True, data=True)]
 
-    # Plot the network
-    fig, ax = ox.plot_graph(g_box, bgcolor='white', edge_color=ec,
-                            node_size=0, edge_linewidth=0.5,
-                            show=False, close=False)
-    # Plot the buildings
-    ox.plot_footprints(building_gdf, ax=ax, color="orange", alpha=0.5)
-    return None, None
+        # Plot the network
+        fig, ax = ox.plot_graph(g_box, bgcolor='white', edge_color=ec,
+                                node_size=0, edge_linewidth=0.5,
+                                show=False, close=False)
+        # Plot the buildings
+        ox.plot_footprints(building_gdf, ax=ax, color="orange", alpha=0.5)
+    return DetailedReport(None, None)
