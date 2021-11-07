f55fc71172c050a1b3ca84b4b7b0ceb8c337b4be
Chris Molanus <chrismolanus@gmail.com>
2021-11-07 12:07:38 +0100

    fix: add trenches to plot

--- trenches2.py
+++ trenches2.py
@@ -1099,19 +1099,22 @@ def add_trenches_to_network(trench_network: TrenchNetwork,
 
 
 if __name__ == "__main__":
-    g_box = ox.graph_from_bbox(50.78694, 50.77902, 4.48386, 4.49521,
+    box = (50.843217, 50.833949, 4.439903, 4.461962)
+    g_box = ox.graph_from_bbox(*box,
                                network_type='drive',
                                simplify=False,
                                retain_all=False,
                                truncate_by_edge=True)
-    building_gdf = ox.geometries_from_bbox(50.78694, 50.77902, 4.48586, 4.49721, tags={'building': True})
+    building_gdf = ox.geometries_from_bbox(*box, tags={'building': True})
     trench_network = get_trench_network(g_box, building_gdf)
+
+    trench_graph = add_trenches_to_network(trench_network, g_box)
 
     ec = ['black' if 'highway' in d else
           "grey" if "trench_crossing" in d and d["trench_crossing"]else
           "blue" if "house_trench" in d else
-          'red' for _, _, _, d in g_box.edges(keys=True, data=True)]
-    fig, ax = ox.plot_graph(g_box, bgcolor='white', edge_color=ec,
+          'red' for _, _, _, d in trench_graph.edges(keys=True, data=True)]
+    fig, ax = ox.plot_graph(trench_graph, bgcolor='white', edge_color=ec,
                             node_size=0, edge_linewidth=0.5,
                             show=False, close=False)
     ox.plot_footprints(building_gdf, ax=ax, color="orange", alpha=0.5)
